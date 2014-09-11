6f938343c104655debbc85592d34607874afcf81
szefo555 <domi.ko@gmx.de>
2014-09-11 11:07:00 +0200

    a

--- serial/bricker.c
+++ serial/bricker.c
@@ -9,7 +9,7 @@
 
 
 /* right, bot, back */
-void CheckEdge(size_t b[3], size_t edge[6], const size_t GDIM, const size_t VOLUME[3], const size_t BRICKSIZE, int src[3]) {
+void CheckEdge(size_t b[3], int edge[6], const size_t GDIM, const size_t VOLUME[3], const size_t BRICKSIZE, int src[3], size_t bno) {
 	/* left top front */
 	for(size_t i=0; i<3; i++) {
 		if(src[i] < 0) {
@@ -20,10 +20,9 @@ void CheckEdge(size_t b[3], size_t edge[6], const size_t GDIM, const size_t VOLU
 	/* right bot back */
 	for(size_t i=0; i<3; i++) {
 		size_t n = VOLUME[i]/BRICKSIZE;
-		size_t cmp = 0;
-		if(VOLUME[i]%BRICKSIZE==0) cmp++;
-		if(b[i] + cmp == n) {
-			edge[i*2+1] = BRICKSIZE+GDIM-(VOLUME[i]%BRICKSIZE);
+		/* NEEDS FIX ASAP */
+		if(b[i]+GDIM >= n) {
+			edge[i*2+1] = VOLUME[i]%BRICKSIZE+GDIM;
 		} 
 	} 
 }
@@ -48,12 +47,17 @@ void ResetOffset(size_t o[3], size_t oo[3]) {
 }
 
 /* edge2 right, bot, back */
-size_t ReadFile(FILE *f, const size_t VOLUME[3], uint8_t *data, int src[3], const size_t GBSIZE, size_t edge[6], size_t bno)
+size_t ReadFile(FILE *f, const size_t VOLUME[3], uint8_t *data, int src[3], const size_t GBSIZE, int edge[6], size_t bno)
 {
+	printf("%d || %d %d %d || %d %d %d %d %d %d \n",bno, src[0], src[1], src[2], edge[0],edge[1],edge[2],edge[3],edge[4],edge[5]);
 	size_t cur = 0;
 	const size_t ORIG_Y = src[1];
 	for(size_t z=0; z<GBSIZE; z++) {
 		for(size_t y=0; y<GBSIZE; y++) {
+			if(src[1] >= VOLUME[1])
+				src[1]--;
+			if(src[2] >= VOLUME[2])
+				src[2]--;
 			/* y_edge top GHOSTCELLS */
 			if(edge[2] > 0 && y < edge[2] && src[1]>0) 
 					src[1]--;
@@ -63,6 +67,7 @@ size_t ReadFile(FILE *f, const size_t VOLUME[3], uint8_t *data, int src[3], cons
 			}	
 			/* z_edge back NO GHOSTCELLS */
 			if(edge[5]>0 &&  src[2] >= VOLUME[2]) {
+				
 				src[2]--;
 			}
 			const size_t LINE = GBSIZE-edge[0]-edge[1];
@@ -119,81 +124,45 @@ size_t ReadVoxel(FILE* f, uint8_t data, size_t off)
 	return 0;
 }
 
-size_t ReadLine(FILE* f, uint8_t *data, size_t LINE, size_t off)
+size_t ReadLine(FILE* f, uint8_t *data, size_t cur, size_t LINE, size_t off)
 {
 	int err;
 	fseek(f, off, SEEK_SET);
-	if(err = fread(&data[0], sizeof(uint8_t), LINE, f) != LINE)
+	if(err = fread(&data[cur], sizeof(uint8_t), LINE, f) != LINE)
 		return 1;
+	cur+=LINE;
 	return 0;
 }
 
-size_t ReadBrick(FILE* f, size_t bID, size_t bno, uint8_t *data, size_t GBSIZE, const size_t GDIM, const size_t edge[6])
-{	
-	size_t cur = 0;
-	const size_t EDGE = GDIM*2;
-	size_t fo = bID*GBSIZE*GBSIZE*GBSIZE;
-	/* left top front */
-	for(size_t z = 0; z<GBSIZE; z++) {
+size_t ReadBrick(FILE* f, uint8_t* data, size_t GBSIZE, size_t GDIM, size_t cur)
+{
+	int err;
+	for(size_t z=0; z<GBSIZE; z++) {
 		for(size_t y=0; y<GBSIZE; y++) {
-			uint8_t *buf_LR = malloc(sizeof(uint8_t) * 2 * EDGE);
-			uint8_t *buf_TB = malloc(sizeof(uint8_t) * 2 * GBSIZE);	
-			uint8_t *buf_FB = malloc(sizeof(uint8_t) * 2 * GBSIZE * GBSIZE);	
-			if(GDIM>0) {
-				if(edge[0]==1) {
-					size_t off = fo + y*GBSIZE + z*GBSIZE*GBSIZE;
-					uint8_t b;
-					if(ReadVoxel(f, b, off) != 0) {
-						printf("ERROR: Reading Brick %d @ Y %d Z %d LEFT\n",bID, y,z);
-						return 1;
-					} 
-					for(size_t i=0; i<EDGE; i++) 
-						data[i+0] = b;
-				}
-				if(edge[1]==1) {
-					size_t off = fo + y*GBSIZE + z*GBSIZE*GBSIZE + GBSIZE-1*GDIM;
-					uint8_t b;
-					if(ReadVoxel(f, b, off) != 0) {
-						printf("ERROR: Reading Brick %d @ Y %d Z %d RIGHT\n",bID, y,z);
-						return 1;
-					}
-					for(size_t i=0; i<EDGE; i++) 
-						data[GBSIZE-GDIM+i];
-				}
-				if(edge[2]==1 && y<EDGE) {
-					size_t off = fo + z*GBSIZE*GBSIZE+y*GBSIZE;
-					if(ReadLine(f, buf_TB, GBSIZE, off) != 0) {
-						printf("ERROR: Reading Brick %d @ Y %d Z %d TOP\n",bID, y,z);
-						return 1;
-					}
-				}
-				if(edge[3]==1 && y>=GBSIZE-EDGE) {
-					size_t off = fo + (GBSIZE-EDGE)*GBSIZE*GBSIZE+y*GBSIZE;
-					if(ReadLine(f, buf_TB, GBSIZE, off) != 0) {
-						printf("ERROR: Reading Brick %d @ Y %d Z %d BOT\n",bID, y,z);
-						return 1;
-					}	
-				}
-				if(edge[4]==1 && z<EDGE) {
-					size_t off = fo + z*GBSIZE*GBSIZE+y*GBSIZE;
-					if(ReadLine(f, buf_FB, GBSIZE*GBSIZE, off) != 0) {
-						printf("ERROR: Reading Brick %d @ Y %d Z %d FRONT\n",bID, y,z);
-						return 1;
-					}
-				}
-				if(edge[5]==1 && z>=GBSIZE-EDGE) {
-					size_t off = fo + (GBSIZE-EDGE)*GBSIZE*GBSIZE+y*GBSIZE;	
-					if(ReadLine(f, buf_FB, GBSIZE*GBSIZE, off) != 0) {
-						printf("ERROR: Reading Brick %d @ Y %d Z %d BACK\n",bID, y,z);
-						return 1;
-					}
-
-				}
-			}
+			fseek(f, cur, SEEK_SET);
+			if(err = fread(&data[cur], sizeof(uint8_t), GBSIZE, f) != GBSIZE)
+				printf("ERROR");
+			printf("%d %d\n",data[cur], cur);
+			cur+=GBSIZE+4*GDIM;
 		}
 	}
-	return 0;	
-		
+}
+
+size_t GetBricks(FILE* f, uint8_t* data, size_t GBSIZE, size_t GDIM, size_t b[3], size_t bpd[3])
+{
+	size_t cur = 0;
+	for(size_t z=0; z<2; z++) {
+		for(size_t y=0; y<2; y++) {
+			for(size_t x=0; x<2; x++) {
+				size_t bc[3] = {x+b[0],y+b[1],z+b[2]}; 
+				fseek(f, GetBrickId(bc,bpd)*GBSIZE*GBSIZE*GBSIZE, SEEK_SET);
+				printf("==== %d %d %d %d %d\n",cur, x,y,z,GetBrickId(bc,bpd)*GBSIZE*GBSIZE*GBSIZE);
+				ReadBrick(f, data, GBSIZE, GDIM, cur);	
+				printf("==== %d %d %d %d %d\n",cur, x,y,z,GetBrickId(bc,bpd)*GBSIZE*GBSIZE*GBSIZE);			
+				cur+=GDIM*GBSIZE*GBSIZE+GDIM*GBSIZE+GDIM+GBSIZE*GBSIZE*GBSIZE;
+			}
+		}	
+	}
 }
 
 void CalcMultiRes(FILE* fpi, FILE* fpo, const size_t BSIZE, const size_t GDIM, size_t VOLUME[3], size_t fo)
@@ -203,21 +172,16 @@ void CalcMultiRes(FILE* fpi, FILE* fpo, const size_t BSIZE, const size_t GDIM, s
 	/* how many bricks are in this lod */
 	size_t numberofbricks = NBricks(VOLUME, 2, bpd);
 	printf("GBSIZE:%d #Bricks%d\n",GBSIZE, numberofbricks);
-	size_t b_number=0;
-	for(size_t i=0; i<numberofbricks;i++) {
-				uint8_t *data = malloc(sizeof(uint8_t) * GBSIZE * GBSIZE* GBSIZE * 8);				
-				ReadBrick(fpi, 0, b_number+0, data, GBSIZE, GDIM, (size_t[6]){1,0,1,0,1,0});
-				ReadBrick(fpi, 1, b_number+1, data, GBSIZE, GDIM, (size_t[6]){0,1,1,0,1,0});
-				ReadBrick(fpi, 2, b_number+bpd[0], data, GBSIZE, GDIM, (size_t[6]){1,0,0,1,0,0});
-				ReadBrick(fpi, 3, b_number+bpd[0]+1, data, GBSIZE, GDIM, (size_t[6]){0,1,0,1,0,0});
-				ReadBrick(fpi, 4, b_number+bpd[0]*bpd[1], data, GBSIZE, GDIM, (size_t[6]){1,0,1,0,1,1});
-				ReadBrick(fpi, 5, b_number+bpd[0]*bpd[1]+1, data, GBSIZE, GDIM, (size_t[6]){0,1,1,0,1,1});
-				ReadBrick(fpi, 6, b_number+bpd[0]*bpd[1]+bpd[0], data, GBSIZE, GDIM, (size_t[6]){1,0,0,1,0,1});
-				ReadBrick(fpi, 7, b_number+bpd[0]*bpd[1]+bpd[0]+1, data, GBSIZE, GDIM, (size_t[6]){0,1,0,1,0,1});
-				b_number+=2;
+	size_t bNO = 0;		
+	for(size_t i=0; i<numberofbricks; i++) {
+		size_t b[3];
+		GetBrickCoordinates(b, bpd, bNO);
+		uint8_t *data = malloc(sizeof(uint8_t) * BSIZE*BSIZE*BSIZE * 8);
+		GetBricks(fpi, data, BSIZE, GDIM, b, VOLUME);
+		uint8_t *odata = malloc(sizeof(uint8_t) * BSIZE*BSIZE*BSIZE * 4);
+		WriteBrick(fpo, data, GBSIZE*GBSIZE*GBSIZE*8, 0);	
+		bNO+=2;
 	}
-
-	
 }
 
 int main(int argc, char* argv[]) 
@@ -266,7 +230,7 @@ int main(int argc, char* argv[])
 	FILE *fpo = NULL;
 	char fn[256];
 	sprintf(fn, "%d.raw", 1);
-	fpo = fopen(fn,"w+b");
+	fpo = fopen(fn,"wb");
 	if(fpo == NULL) {
 		printf("Couldn't open file %s.raw\n", fn);
 		return EXIT_FAILURE;
@@ -282,9 +246,8 @@ int main(int argc, char* argv[])
 			 (b[1]%bricks_per_dimension[1]*BRICKSIZE)-GHOSTCELLDIM,
 			 (b[2]%bricks_per_dimension[2]*BRICKSIZE)-GHOSTCELLDIM};
 
-		size_t edge[6] = {0,0,0,0,0,0};
-		CheckEdge(b, edge, GHOSTCELLDIM, VOLUME, BRICKSIZE, src);
-		printf("%d %d %d\n", src[0], src[1], src[2]);
+		int edge[6] = {0,0,0,0,0,0};
+		CheckEdge(b, &edge[0], GHOSTCELLDIM, VOLUME, BRICKSIZE, src, no_b);
 		uint8_t *data = malloc(sizeof(uint8_t) * GBSIZE * GBSIZE * GBSIZE);		
 		if(ReadFile(fpi, VOLUME, data, src, GBSIZE, edge, no_b) != 0) 
 			return EXIT_FAILURE;
@@ -299,7 +262,7 @@ int main(int argc, char* argv[])
 	fpo = fopen(fn,"rb");
 
 	FILE *fpm = NULL;
-	fpm = fopen("multi.raw", "w+b");
+	fpm = fopen("xmulti.raw", "w+b");
 	if(fpm == NULL) {
 		printf("Couldn't open file multi.raw\n");
 		return EXIT_FAILURE;
