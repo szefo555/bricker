3cf14538ab503edbb8d6638692f879e16f7d397b
szefo555 <domi.ko@gmx.de>
2014-09-22 19:11:19 +0200

    f

--- parallel/file_io.h
+++
@@ -1,6 +0,0 @@
-#ifndef file_io_h
-#define file_io_h
-
-size_t OpenFile(MPI_File, char*, size_t);
-
-#endif
--- parallel/rw.h
+++
@@ -1,8 +0,0 @@
-#ifndef rw_h
-#define rw_h
-
-void Seek(MPI_File, int);
-void Padding(MPI_File, size_t);
-void CopyLine(MPI_File, MPI_File, int, size_t, int*);
-
-#endif
